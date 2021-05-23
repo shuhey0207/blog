5af1f7ec5f9465e7581f6ddbdedbdab336e7862f
shuhei <shuhey.1622@gmail.com>
2021-05-23 23:06:32 +0900

    set up

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+
+
+module.exports = {
+    title: 'SCODER',
+    base: '/blog/',
+    description: 'とあるソフトウェアエンジニアの記録',
+    locales: {
+        '/': {
+          lang: 'ja'
+        }
+    },
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Github', link: 'https://github.com/shuhey0207' }
+      ],
+      sidebarDepth: 0,
+      sidebar: [
+        {
+          title: 'Test',
+          children: [
+            {
+              title: 'Test sub',
+              children: [
+                '/test/test_sub/test_sub1/',
+                '/test/test_sub/test_sub2/'
+              ]
+            }
+          ]
+        }
+      ],
+    }
+  }
---
+++ docs/test/test_sub/test_sub1/index.md
@@ -0,0 +1,1 @@
+# Test Sub 1
